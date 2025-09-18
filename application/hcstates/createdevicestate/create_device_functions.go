3e5444fcbe74970233089e8434019e6a3d42a19a
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2025-09-19 01:05:20 +0200

    fix: Refactor new device  logic

--- application/hcstates/createdevicestate/create_device_functions.go
+++ application/hcstates/createdevicestate/create_device_functions.go
@@ -1,8 +1,10 @@
 package createdevicestate
 
 import (
-	"fmt"
+	"strings"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/janicaleksander/bcs/types/proto"
 	"github.com/janicaleksander/bcs/utils"
 )
@@ -13,11 +15,12 @@ func (d *CreateDeviceScene) Reset() {
 
 func (d *CreateDeviceScene) FetchUsers() {
 	res, err := utils.MakeRequest(utils.NewRequest(d.cfg.Ctx, d.cfg.ServerPID, &proto.GetUserAboveLVL{
-		Lower: -1,
-		Upper: 10,
-	})) //TODO
+		Lower: 0,
+		Upper: 3,
+	}))
 	if err != nil {
-		// context deadline exceeded
+		d.errorSection.error = err.Error()
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
 		return
 	}
 	d.newDeviceSection.users = make([]*proto.User, 0, 64)
@@ -29,7 +32,10 @@ func (d *CreateDeviceScene) FetchUsers() {
 				user.Personal.Name, user.Personal.Surname+"\n"+user.Email)
 		}
 	} else {
-		// TODO error
+		v, _ := res.(*proto.Error)
+		d.errorSection.error = v.Content
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
+		return
 	}
 
 }
@@ -37,73 +43,59 @@ func (d *CreateDeviceScene) FetchUsers() {
 func (d *CreateDeviceScene) FetchDeviceTypes() {
 	res, err := utils.MakeRequest(utils.NewRequest(d.cfg.Ctx, d.cfg.ServerPID, &proto.GetDeviceTypes{}))
 	if err != nil {
-		//context deadline error
+		d.errorSection.error = err.Error()
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
 		return
 	}
 	d.newDeviceSection.deviceTypes = make([]int, 0, 64)
-	//d.newDeviceSection.typeSlider.Strings = make([]string, 0, 64)
 	if v, ok := res.(*proto.DeviceTypes); ok {
 		for _, t := range v.Types {
 			d.newDeviceSection.deviceTypes = append(d.newDeviceSection.deviceTypes, int(t))
-			//d.newDeviceSection.typeSlider.Strings = append(d.newDeviceSection.typeSlider.Strings, strconv.Itoa(int(t)))
 		}
 	} else {
-		fmt.Println(err)
-		//error
+		v, _ := res.(*proto.Error)
+		d.errorSection.error = v.Content
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
+		return
 	}
 
 }
 
 func (d *CreateDeviceScene) CreateDevice() {
-	//add to device
-	/*
-		name := d.newDeviceSection.nameInput.GetText()
-		currOwnerIdx := d.newDeviceSection.ownerSlider.IdxActiveElement
-		currTypeIdx := d.newDeviceSection.typeSlider.IdxActiveElement
-		if len(strings.TrimSpace(name)) == 0 || currTypeIdx == -1 || currOwnerIdx == -1 {
-			d.errorSection.error = "Bad inputs"
-			d.errorSection.errorPopup.Show()
-			d.scheduler.After((3 * time.Second).Seconds(), func() {
-				d.errorSection.errorPopup.Hide()
-			})
-			return
-		}
-		id := uuid.New()
-		owner := d.newDeviceSection.users[currOwnerIdx].Id
-		t := d.newDeviceSection.deviceTypes[currTypeIdx]
-		device := &proto.Device{
-			Id:             id.String(),
-			Name:           name,
-			Owner:          owner,
-			LastTimeOnline: nil,
-			Type:           int32(t),
-		}
+	name := d.newDeviceSection.nameInput.GetText()
+	currOwnerIdx := d.newDeviceSection.ownerSlider.IdxActiveElement
+	currTypeIdx := d.newDeviceSection.typesToggle.Selected
+	if len(strings.TrimSpace(name)) == 0 || currOwnerIdx == -1 {
+		d.errorSection.error = "Bad inputs"
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
+		return
+	}
+	id := uuid.New()
+	owner := d.newDeviceSection.users[currOwnerIdx].Id
+	t := d.newDeviceSection.deviceTypes[currTypeIdx]
+	device := &proto.Device{
+		Id:             id.String(),
+		Name:           name,
+		Owner:          owner,
+		LastTimeOnline: nil,
+		Type:           int32(t),
+	}
 
-		res, err := utils.MakeRequest(
-			utils.NewRequest(d.cfg.Ctx, d.cfg.ServerPID,
-				&proto.CreateDevice{Device: device}))
+	res, err := utils.MakeRequest(
+		utils.NewRequest(d.cfg.Ctx, d.cfg.ServerPID,
+			&proto.CreateDevice{Device: device}))
 
-		if err != nil {
-			d.errorSection.error = "Ctx error"
-			d.errorSection.errorPopup.Show()
-			d.scheduler.After((3 * time.Second).Seconds(), func() {
-				d.errorSection.errorPopup.Hide()
-			})
-		}
+	if err != nil {
+		d.errorSection.error = "Ctx error"
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
+		return
+	}
 
-		if _, ok := res.(*proto.AcceptCreateDevice); ok {
-			d.infoSection.info = "Good!"
-			d.infoSection.infoPopup.Show()
-			d.scheduler.After((3 * time.Second).Seconds(), func() {
-				d.infoSection.infoPopup.Hide()
-			})
-		} else {
-			d.errorSection.error = "Answer error"
-			d.errorSection.errorPopup.Show()
-			d.scheduler.After((3 * time.Second).Seconds(), func() {
-				d.errorSection.errorPopup.Hide()
-			})
-		}
-
-	*/
+	if _, ok := res.(*proto.AcceptCreateDevice); ok {
+		d.infoSection.info = "Good!"
+		d.infoSection.infoPopup.ShowFor(time.Second * 3)
+	} else {
+		d.errorSection.error = "Answer error"
+		d.errorSection.errorPopup.ShowFor(time.Second * 3)
+	}
 }
